70cee805c955ca7a2cb06d66b1c992a73dc622e0
Greg Trifan <55629140+TheSlayer-666@users.noreply.github.com>
2021-03-12 15:16:56 +0200

    Refractors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,38 @@
-import { Box, Menu } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import {
+  Box,
+  Menu,
+  Button,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  useColorMode,
+  DarkMode
+} from "@chakra-ui/react";
+import { CgMenu } from "react-icons/cg";
 const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Box bg="black" color="#FFFFFF" padding={3}>
-      <span>Logo</span>
-      <Box float="right">
-        <Link to="/">Home </Link>
-        <Link to="/about">About</Link>
+    <DarkMode>
+      <Box bg="black" color="#FFFFFF" padding={6}>
+        <Menu>
+          <MenuButton
+            border="1px"
+            _hover={{ bg: "#121212" }}
+            bg="black"
+            as={Button}
+          >
+            <CgMenu />
+          </MenuButton>
+          <MenuList>
+            <MenuItem to="/">Download</MenuItem>
+            <MenuItem to="/about">Create a Copy</MenuItem>
+            <MenuItem onClick={toggleColorMode}>
+              {colorMode === "light" ? "Go Dark 🌙" : "Go Light 🌞"}
+            </MenuItem>
+          </MenuList>
+        </Menu>
       </Box>
-    </Box>
+    </DarkMode>
   );
 };
 
